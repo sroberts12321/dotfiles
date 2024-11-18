a88728519387dfffc7be16c555b9f698cd5e9024
root <root@Stephen.>
2024-11-17 19:06:03 -0600

    v1.0 nvim config

---
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,22 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {
+    window = {
+      width = 25,
+    },
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+      },
+    },
+  },
+  keys = {
+    { "<C-n>", ":Neotree filesystem reveal left<CR>" },
+  },
+}
